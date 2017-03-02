281ba216db1735b27fbed9d763d35cfdbaabeae1
Melbourne Genomics Health Alliance <mgha@mgha.novalocal>
2017-03-02 22:52:49 +0000

    Uncommented compressor.finder in settings.py

--- web/pumi/pumi/settings.py
+++ web/pumi/pumi/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #'compressor.finders.CompressorFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
